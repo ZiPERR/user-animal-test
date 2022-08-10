4308a002c6032df30a147329985fdea94d426fd8
ZiPER <shadowrunner730@gmail.com>
2022-08-10 20:01:14 +0300

    merged UserService.java with CustomUserDetailsService.java

--- src/main/java/artplancom/test/services/CustomUserDetailsService.java
+++ src/main/java/artplancom/test/services/CustomUserDetailsService.java
@@ -1,27 +1,24 @@
 package artplancom.test.services;
 
-import artplancom.test.models.Role;
+import artplancom.test.models.Animal;
 import artplancom.test.models.User;
+import artplancom.test.repositories.RolesRepository;
 import artplancom.test.repositories.UsersRepository;
 import artplancom.test.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
-    private UsersRepository usersRepository;
+    private final UsersRepository usersRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -30,5 +27,44 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("User not found.");
         }
         return new CustomUserDetails(user.get());
+    }
+
+    @Autowired
+    private RolesRepository rolesRepository;
+
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<User> findAll() {
+        return usersRepository.findAll();
+    }
+
+    public Optional<User> findById(Long userId) {
+        return usersRepository.findById(userId);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return usersRepository.findByUsername(username);
+    }
+
+    public User save(User user){
+        return usersRepository.save(user);
+    }
+
+    public void delete(User user) {
+        usersRepository.delete(user);
+    }
+
+    public User addAnimalToUser(User user, Animal animal) {
+        user.getAnimals().add(animal);
+        animal.setUser(user);
+        return usersRepository.save(user);
+    }
+
+    public void deleteAnimalFromUser(User user, Animal animal) {
+        user.getAnimals().remove(animal);
+        animal.setUser(null);
+        usersRepository.save(user);
     }
 }
